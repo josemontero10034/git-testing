ca2e0b69332f1610ebfa8a9dfe6a6f5f569615a3
jose montero <jose142578@gmail.com>
2022-05-02 10:58:42 -0400

    este es el archivo completo

---
+++ customer-data.js
@@ -0,0 +1,18 @@
+const customer = [{ 
+    id: '1',
+    name: 'George Montero',
+},
+{ 
+    id: '2',
+    name: 'Peter Pan',
+},
+
+{ 
+    id: '3',
+    name: 'Juan Marichala',
+},
+
+{ 
+    id: '4',
+    name: 'Pedro Navaja',
+}]
